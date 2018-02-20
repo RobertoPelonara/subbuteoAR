0926137bc3b52f20ab826ca308d4868211a503df
Alfonso <alfonsotesone@mbp-di-alfonso.fritz.box>
2018-02-20 17:38:39 +0100

    Grafica menu

--- subbuteoAR/Additional View Controllers/peerViewController.swift
+++ subbuteoAR/Additional View Controllers/peerViewController.swift
@@ -41,6 +41,20 @@ class peerViewController: UIViewController, MPCManagerDelegate {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     @IBAction func joinAction(_ sender: UIButton) {
         MPCManager.shared.isHost = false
     }
